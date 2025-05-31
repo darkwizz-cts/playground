4ddfeaf15b56a1cc1815500947ab128948427117
someone <some@gmail.com>
2025-05-31 08:55:58 +0530

    Add new

---
+++ Sandbox.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+
+public class Sandbox {
+    public static void main(String[] args) {
+        LocalTime now = LocalTime.now();
+        System.out.println(now);
+    }
+}
